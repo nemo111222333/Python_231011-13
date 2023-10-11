0acd1cd0e68531b06f720e188e91364390966654
nemo123 <nemo123@interia.com>
2023-10-11 10:59:52 +0200

    11 10:45

--- main.py
+++ main.py
@@ -1,2 +1,66 @@
 # Szkolenie Podstawy Pythona 2023-10-11 do 13
 
+# print("Cześć 'test'")
+#
+# print('Witaj "test"')
+
+# print(10)
+#
+# print(10.5)
+#
+# print(10+1)
+
+# print(10.5 + 2.5)
+#
+# print("Ala " + "ma " + "kota")
+#
+# print("Ala" * 10)
+#
+# print(2 ** 3)
+
+# liczba1 = 10
+# print(liczba1 * 2)
+
+# liczba1 = 10.5
+# print(liczba1*2)
+#
+# liczba1 = "ala"
+# print(liczba1)
+
+# numberOne = 10
+# print(numberOne)
+#
+# print(liczba1)
+#
+# print(liczba1 == numberOne)
+#
+# print(liczba1 != numberOne)
+# liczba1 = 100
+# print(liczba1 == 100)
+
+# warunek = True
+# warunek = False
+#
+# warunek1 = True
+# warunek2 = warunek1 and warunek
+# print(warunek2)
+
+warunek, warunek1, warunek2 = True, False, 1
+print(warunek, warunek1, warunek2)
+
+lista1 = [1, 2, 3]
+
+print(lista1, lista1[0])
+
+lista2 = ["ala", "ma", "kota", 1, 1, 4.5, True]
+print(lista2)
+
+krotka = ("ala", "ma", "kota", 1, 1, 4.5, True)
+print(krotka)
+
+lista2.append(10)
+print(lista2)
+
+print(krotka[2])
+print(len(krotka))
+print(len(lista2))
