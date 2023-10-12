c8e7aa264e0655dc2a47f24a1009679ac1c0a444
nemo123 <nemo123@interia.com>
2023-10-12 09:44:47 +0200

    12 9:44

--- dzien1/l5.py
+++ dzien1/l5.py
@@ -1,6 +1,8 @@
 import os
 import matma
 import matma as m
+import random
+import sys
 
 # sciezka = os.getcwd()
 # print(sciezka)
@@ -14,19 +16,31 @@ import matma as m
 # print(m.dodaj("ala","kot"))
 
 
+odp = ["odpowiedź 1", "odpowiedź 2", "odpowiedź 3", "odpowiedź 4", "odpowiedź 5", "odpowiedź 6", "odpowiedź 7",
+       "odpowiedź 8", "odpowiedź 9", "odpowiedź 10"]
 def answer(question):
     # return "odpowiedz"
-    return len(question)
+    #return len(question)
+    return random.choice(odp)
+
+
+def przetworz(lines, wynik=sys.stdout, blad=sys.stderr):
+    i = 1
+    for line in lines:
+        if line:
+            print(f"{line} => {answer(line)}", file=wynik)
+        else:
+            print(f"błąd w linii nr {i}", file=blad)
+        i += 1
 
 
 plik = open("in.txt", "r", encoding="utf-8")
 data = plik.read()
-lines = data.split("\n")
-i = 1
-for line in lines:
-    if line:
-        print(f"{line} => {answer(line)}")
-    else:
-        print(f"błąd w linii nr {i}")
-    i += 1
 plik.close()
+lines = data.split("\n")
+wynik = open("out.txt", "w", encoding="utf-8")
+blad = open("error.txt", "w", encoding="utf-8")
+# przetworz(lines, wynik, blad)
+przetworz(lines, wynik=wynik, blad=blad)
+wynik.close()
+blad.close()
