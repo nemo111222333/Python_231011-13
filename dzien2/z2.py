220c41a7f5ef726cbe04a37b339cd1af0d5d8d45
nemo123 <nemo123@interia.com>
2023-10-12 13:10:09 +0200

    12 13:10

--- dzien2/z2.py
+++ dzien2/z2.py
@@ -1,6 +1,16 @@
-artykuly = {'a1':("artykuł1", 5.0), 'a2':("artykuł2", 10.0), 'a3':("Karty do gry", 20.0)}
+artykuly = {'a1': ("artykuł1", 5.0), 'a2': ("artykuł2", 10.0), 'a3': ("Karty do gry", 20.0)}
 
-a = input("Proszę podać artykuł: ")
-print(artykuly[a][0])
-print(artykuly[a][1])
+while True:
+    a = input("Proszę podać artykuł: ")
+    if a in artykuly:
+        i = input("Proszę podać ilość: ")
+        print(artykuly[a][0])
+        print(artykuly[a][1])
+    elif a == "":
+        print("Dziękujemy za skorzystanie z programu")
+        break
+    else:
+        print("Artykuł nie istnieje")
 
+# ToDo: Przygotować funkcję wypisującą w jednej linii elementy:
+# lp, artykul, ilosc, cena jednostkowa, wartosc
